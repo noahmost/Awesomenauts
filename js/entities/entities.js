f9473e10f9638a670612257486c9a17a23f7924e
Noah Mostacero <noahmostacero@gmail.com>
2015-01-23 11:29:06 -0800

    Video 7 code done

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -13,11 +13,20 @@ game.PlayerEntity = me.Entity.extend ({
             
             
                 }]);
+            
+            this.body.setVelocity(5, 0);
             },
         
         
                     
-            update: function(){
-            
+            update: function(delta){
+            if(me.input.isKeyPressed("right")){
+
+                    this.body.vel.x += this.body.accel.x * me.timer.tick;
+            }else{
+                this.body.vel.x=0;
+            }
+            this.body.update(delta);
+            return true;
             }
 });
--- js/screens/play.js
+++ js/screens/play.js
@@ -10,6 +10,8 @@ game.PlayScreen = me.ScreenObject.extend({
 //                this sets and adds the player entity
                 var player = me.pool.pull("player", 0, 420, {});
                 me.game.world.addChild(player, 5);
+                
+                me.input.bindKey(me.input.KEY.RIGHT, "right");
 
 		// add our HUD to the game world
 		this.HUD = new game.HUD.Container();
