e52ee5d388293cd7a3c032fbac549f41edac4bc0
Noah Mostacero <noahmostacero@gmail.com>
2015-01-30 11:08:06 -0800

    Finished video 10 edited animations

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -15,6 +15,11 @@ game.PlayerEntity = me.Entity.extend ({
                 }]);
 //            this sets the speed that the character is going
             this.body.setVelocity(10, 20);
+            
+            this.renderable.addAnimation("idle", [78]);
+            this.renderable.addAnimation("walk", [117, 118, 119, 120, 121, 123, 124, 125], 80);
+            
+            this.renderable.setCurrentAnimation("idle");
             },
         
         
@@ -23,13 +28,25 @@ game.PlayerEntity = me.Entity.extend ({
             if(me.input.isKeyPressed("right")){
 //                velocity represents our current position, sets position of x by multiplying velocity by me.timer.tick
                     this.body.vel.x += this.body.accel.x * me.timer.tick;
+                    this.flipX(true);
             }else if(me.input.isKeyPressed("left")){
                 this.body.vel.x -= this.body.accel.x * me.timer.tick;
+                this.flipX(false);
             }
             else{
                 this.body.vel.x=0;
             }
+            if(this.body.vel.x !== 0){
+                    if(!this.renderable.isCurrentAnimation("walk")){
+                        this.renderable.setCurrentAnimation("walk");
+                    }
+        }else{
+            this.renderable.setCurrentAnimation("idle");
+        }
             this.body.update(delta);
+            
+            this._super(me.Entity, "update", [delta]);
+            
             return true;
             }
 });
